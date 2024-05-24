d3ddd5fc58d3c410404910ead17b742ccea32993
Sasha Khapko <sxapko2211@gmail.com>
2024-05-24 11:37:25 +0300

    first commit

---
+++ css/main.js
@@ -0,0 +1,37 @@
+let qoue = document.querySelector('.par');
+
+function one() {
+    qoue.innerHTML = 'Народна пісня-духовне обличчя нації';
+    setTimeout(() => {
+        alert('Автор: А. Міцкевич');
+    }, 100);
+}
+
+function second() {
+    qoue.innerHTML = 'Українська пісня — це бездонна душа українського народу, це його слава';
+    setTimeout(() => {
+        alert('Автор: О. Довженко');
+    }, 100);
+}
+
+function third() {
+    qoue.innerHTML = 'Я єсть народ, якого Правди сила ніким звойована ще не була.';
+    setTimeout(() => {
+        alert('Автор: П. Тичина');
+    }, 100);
+}
+
+function four() {
+    qoue.innerHTML = 'Немає магії сильнішої, ніж магія слів.';
+    setTimeout(() => {
+        alert('Автор: А. Франс');
+    }, 100);
+}
+
+function five() {
+    qoue.innerHTML = 'Споконвіку було Слово.';
+    setTimeout(() => {
+        alert('Автор: Старий Заповіт');
+    }, 100);
+}
+
